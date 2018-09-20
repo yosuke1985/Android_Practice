36d3f5654cb2af7078951443718dd04201648e0f
yosuke1985 <ihatenonoildressing@gmail.com>
2018-09-20 15:10:47 +0900

    first commit

---
+++ app/src/main/java/com/example/yosukenakayama/getstartkotlin/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.yosukenakayama.getstartkotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var greeting = "こんにちは"
+        val trialTime = Date()
+        val calendar = GregorianCalendar()
+        calendar.time = trialTime
+
+        val hour = calendar.get(Calendar.HOUR_OF_DAY)
+
+        if (hour in 1..11){
+
+            greeting = "おはよう"
+        }else if (15<hour){
+            greeting = "こんばんは"
+        }
+        textview.text = greeting
+    }
+}
